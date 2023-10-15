2c4258368f29a339d05766e62d90e0d02f07f7ab
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-15 17:52:54 -0400

    Progress

--- main.py
+++ main.py
@@ -10,24 +10,47 @@ class ToDo(BaseModel):
     situation:bool
     limit:date
 
+task = {
+    'name':'Jorge',
+    'level':1,
+    'situation':False,
+    'limit':'12-12-2023'
+}
+tasks_list.append(task)
 @app.post('/task_register')
 def register(task:ToDo):
     try:
         tasks_list.append(task)
-        return{'status':'success'}
+        return{'status':'The task is added on your task bank'}
     except:
         return{'status':'error'}
 
 @app.post('/list_tasks')
-def list_tasks(option: int = 0 ):
+def list_tasks(option: int = 1 ):
     try:
-        if(option == 0):
+        if(option == 1):
             return tasks_list
-        elif(option == 1):#just list the tasks already maded
+        elif(option == 2):#just list the tasks already maded
             return list(filter(lambda x:x.situation == True, tasks_list))
-        elif(option == 2):
+        elif(option == 3):
             return list(filter(lambda x:x.situation == False, tasks_list))
-
     except:
         return{'status':'error'}
+    
+@app.post('/list_unique')
+def list_one(id: int):
+    try:
+        return tasks_list[id]
+    except:
+        return{'status':'error'}
+
+@app.post('/alter_task')
+def alter(id:int):
+    try:
+        tasks_list[id].situation = not tasks_list[id].situation
+        return{'status':'The task situation was alter'}
+    except:
+        return{'status':'error'}
+
+
 
--- reqs.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-returning = requests.get('http://127.0.0.1:8000/')
-print(returning)
